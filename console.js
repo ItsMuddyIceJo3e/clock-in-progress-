b45de8ee5d09e0b85b7264f8e87839973533da35
ItsMuddyIceJo3e <255244221+ItsMuddyIceJo3e@users.noreply.github.com>
2026-05-30 00:28:26 -0400

    Add files via upload

---
+++ console.js
@@ -0,0 +1,12 @@
+console.log(hello)
+ <Script>   
+ function updateCustomClock(12%) {
+  const now = new Date(1000);
+    }   
+     document.getElementById('live-clock').textContent = now.toLocaleTimeString();
+     }      
+         setInterval(updateCustomClock, 1000);
+
+            const ampm = hours >= 12 ? 'pm':'am';
+            hours = hours % 12;
+          
